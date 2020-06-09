bb652820ed10a698d4396dde101db4f619d9d818
Jorge Luis Calleja <jorgelca1205@gmail.com>
2020-06-08 21:48:44 -0500

    feat: Update package simple-icons

---
+++ src/components/Virustotal.svelte
@@ -0,0 +1,12 @@
+
+    <script>
+      export let color = 'currentColor';
+      export let size = 24;
+      export let title = "VirusTotal";
+    </script>
+
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={color} viewBox="0 0 24 24">
+      <title>{title}</title>
+      <path d="M10.87 12L0 22.68h24V1.32H0zm10.73 8.52H5.28l8.637-8.448L5.28 3.48H21.6z" />
+    </svg>
+  
