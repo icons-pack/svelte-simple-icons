09a887187f1142150fe023fa34c38a6218edf099
Jorge Luis Calleja <jorgelca1205@gmail.com>
2020-07-06 23:49:38 -0500

    feat: Update package simple-icons

---
+++ src/components/Facebookgaming.svelte
@@ -0,0 +1,12 @@
+
+    <script>
+      export let color = 'currentColor';
+      export let size = 24;
+      export let title = "Facebook Gaming";
+    </script>
+
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={color} viewBox="0 0 24 24">
+      <title>{title}</title>
+      <path d="M0 0v24h15.67v-7.35H7.35v-9.3H24V0zm8.33 15.68h8.32V24H24V8.32H8.33Z" />
+    </svg>
+  
