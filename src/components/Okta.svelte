8c5c550ba76164e5a8cea2015b67be29b58dc884
Jorge Calleja - wootsbot <jorgelca1205@gmail.com>
2021-02-19 10:50:19 -0600

    [Update]: Update simple-icons to 4.11.0

---
+++ src/components/Okta.svelte
@@ -0,0 +1,12 @@
+
+    <script>
+      export let color = 'currentColor';
+      export let size = 24;
+      export let title = "Okta";
+    </script>
+
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={color} viewBox="0 0 24 24">
+      <title>{title}</title>
+      <path d="M12 0C5.389 0 0 5.35 0 12s5.35 12 12 12 12-5.35 12-12S18.611 0 12 0zm0 18c-3.325 0-6-2.675-6-6s2.675-6 6-6 6 2.675 6 6-2.675 6-6 6z" />
+    </svg>
+  
---
+++ src/components/Suckless.svelte
@@ -0,0 +1,12 @@
+
+    <script>
+      export let color = 'currentColor';
+      export let size = 24;
+      export let title = "suckless";
+    </script>
+
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={color} viewBox="0 0 24 24">
+      <title>{title}</title>
+      <path d="M0 4h24v4H4v2h20v10H0v-4h20v-2H0z" />
+    </svg>
+  
