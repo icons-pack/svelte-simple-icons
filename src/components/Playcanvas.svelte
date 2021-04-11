c765b3c4c89c5ee9c84bac8d5a55dae672226fb9
Jorge Luis Calleja Alvarado <jorgelca1205@gmail.com>
2021-04-10 20:12:14 -0500

    feat(update): update package simple-icons to 4.18.0

---
+++ src/components/Playcanvas.svelte
@@ -0,0 +1,12 @@
+
+    <script>
+      export let color = 'currentColor';
+      export let size = 24;
+      export let title = "PlayCanvas";
+    </script>
+
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={color} viewBox="0 0 24 24">
+      <title>{title}</title>
+      <path d="M6.115 0l-.002 3.414 5.823 3.41-5.82 3.414-.003 3.412 11.774-6.826zm11.77 10.35L6.113 17.174 17.887 24l-.002-3.414-5.82-3.412 5.822-3.412z" />
+    </svg>
+  
---
+++ src/components/Xstate.svelte
@@ -0,0 +1,12 @@
+
+    <script>
+      export let color = 'currentColor';
+      export let size = 24;
+      export let title = "XState";
+    </script>
+
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={color} viewBox="0 0 24 24">
+      <title>{title}</title>
+      <path d="M15.891 0h6.023l-6.085 10.563c-1.853-3.305-1.822-7.32.062-10.563zm6.055 23.999L8.078.001H2.055l6.919 12.015L2.055 24h6.023L12 17.236 15.892 24z" />
+    </svg>
+  
